788bb0bee09dd1c37d015e24c1be77f3f634ec5e
jonasfreyr <jonasfreyrbjarnason@gmail.com>
2020-09-03 09:56:37 +0000

    Byrja á dæmi 2

---
+++ sequence.py
@@ -0,0 +1,8 @@
+'''
+    tek in input frá notanda um hversu marga tölur
+
+    geri for lykkju sem keyrir eins oft og inputtið
+        
+'''
+
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
